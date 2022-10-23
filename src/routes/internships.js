cef3f6f653ad433ece096d27912a98b4fa7d4ace
Roel Tombozafy <rtombozafy96@gmail.com>
2022-10-23 09:21:48 +0300

    Changed auth controller

--- src/routes/internships.js
+++ src/routes/internships.js
@@ -8,7 +8,10 @@ const {
   createInternship,
 } = require("../controllers/internships");
 
-router.route("/").get(getAllInternships).post(createInternship);
+router
+  .route("/")
+  .get(authController.checkAuth, getAllInternships)
+  .post(authController.checkAuth, createInternship);
 router.route("/:id").get(authController.checkAuth, getOneInternship);
 
 module.exports = router;
